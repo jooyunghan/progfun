4fbd66c50a58f6847bc130ada831877b4cff7aa9
jooyung.han <jooyunghan@merope.local>
2013-11-21 15:48:50 +0900

    Add quickcheck

---
+++ quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -0,0 +1,30 @@
+package quickcheck
+
+import common._
+
+import org.scalacheck._
+import Arbitrary._
+import Gen._
+import Prop._
+
+abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
+
+  property("min1") = forAll { a: Int =>
+    val h = insert(a, empty)
+    findMin(h) == a
+  }
+
+  def toHeap(l: List[Int]) : H = {
+    l.foldLeft(empty)((h, a) => insert(a, h))
+  }
+  def toList(h: H) : List[Int] = 
+    if (isEmpty(h)) Nil
+    else findMin(h) :: toList(deleteMin(h))
+
+  property("sorted") = forAll { l: List[Int] =>
+    val l2 = toList(toHeap(l))
+    l2 == l.sorted
+  }
+
+
+}
