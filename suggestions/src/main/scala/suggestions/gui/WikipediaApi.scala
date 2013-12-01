27c90d9b313d852623b90155eb77f9a6e39d454b
Jooyung Han <jooyung.han@gmail.com>
2013-12-02 06:59:58 +0900

    Observable.concatRecovered

--- suggestions/src/main/scala/suggestions/gui/WikipediaApi.scala
+++ suggestions/src/main/scala/suggestions/gui/WikipediaApi.scala
@@ -100,7 +100,9 @@ trait WikipediaApi {
      *
      * Observable(Success(1), Succeess(1), Succeess(1), Succeess(2), Succeess(2), Succeess(2), Succeess(3), Succeess(3), Succeess(3))
      */
-    def concatRecovered[S](requestMethod: T => Observable[S]): Observable[Try[S]] = ???
+    def concatRecovered[S](requestMethod: T => Observable[S]): Observable[Try[S]] = {
+      obs.map(requestMethod(_).recovered).concat
+    }
 
   }
 
