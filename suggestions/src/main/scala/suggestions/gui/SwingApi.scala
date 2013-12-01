d09ec16a6615f7d1b684dde87593af2296bb3c4b
Jooyung Han <jooyung.han@gmail.com>
2013-12-02 03:27:59 +0900

    change SwingEvent to Observable

--- suggestions/src/main/scala/suggestions/gui/SwingApi.scala
+++ suggestions/src/main/scala/suggestions/gui/SwingApi.scala
@@ -10,15 +10,18 @@ import scala.util.{ Try, Success, Failure }
 import scala.swing.Reactions.Reaction
 import scala.swing.event.Event
 import rx.lang.scala.Observable
+import rx.Observer
+import rx.lang.scala.subscriptions.Subscription
 
-/** Basic facilities for dealing with Swing-like components.
-*
-* Instead of committing to a particular widget implementation
-* functionality has been factored out here to deal only with
-* abstract types like `ValueChanged` or `TextField`.
-* Extractors for abstract events like `ValueChanged` have also
-* been factored out into corresponding abstract `val`s.
-*/
+/**
+ * Basic facilities for dealing with Swing-like components.
+ *
+ * Instead of committing to a particular widget implementation
+ * functionality has been factored out here to deal only with
+ * abstract types like `ValueChanged` or `TextField`.
+ * Extractors for abstract events like `ValueChanged` have also
+ * been factored out into corresponding abstract `val`s.
+ */
 trait SwingApi {
 
   type ValueChanged <: Event
@@ -46,23 +49,42 @@ trait SwingApi {
 
   implicit class TextFieldOps(field: TextField) {
 
-    /** Returns a stream of text field values entered in the given text field.
-      *
-      * @param field the text field
-      * @return an observable with a stream of text field updates
-      */
-    def textValues: Observable[String] = ???
-
+    /**
+     * Returns a stream of text field values entered in the given text field.
+     *
+     * @param field the text field
+     * @return an observable with a stream of text field updates
+     */
+    def textValues: Observable[String] = Observable { observer =>
+      val r: Reaction = {
+        case ValueChanged(e) => observer onNext e.text
+      }
+      field subscribe r
+      Subscription {
+        field unsubscribe r
+        println("textfield unsubscribe")
+      }
+    }
   }
 
   implicit class ButtonOps(button: Button) {
 
-    /** Returns a stream of button clicks.
+    /**
+     * Returns a stream of button clicks.
      *
      * @param field the button
      * @return an observable with a stream of buttons that have been clicked
      */
-    def clicks: Observable[Button] = ???
+    def clicks: Observable[Button] = Observable { observer =>
+      val r: Reaction = {
+        case ButtonClicked(e) => observer onNext button
+      }
+      button subscribe r
+      Subscription {
+        button unsubscribe r
+        println("button unsubscribe")
+      }
+    }
 
   }
 
